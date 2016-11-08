456ea9b0e7eea12d20d75dc160ee004611d8bf30
Lanza <dev@lanza.fr>
2016-11-08 09:28:41 +0100

    Commit initial

---
+++ server/controllers/index.js
@@ -0,0 +1,16 @@
+module.exports.index = function (req, res, next) {
+    if( req.accepts('image/jpeg') ) {
+          var options = {
+            root: __dirname + '/../../client/public/',
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
+
+        res.sendFile('/vieux.jpg', options);
+    }
+    else
+        res.send('Good bye');
+};
