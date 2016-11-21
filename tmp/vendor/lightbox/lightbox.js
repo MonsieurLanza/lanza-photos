91d4cba5cf64d80b7b7bacf6c5cb11c71abcdd59
Lanza <dev@lanza.fr>
2016-11-21 22:57:24 +0100

    Fix double lightbox background

--- tmp/vendor/lightbox/lightbox.js
+++ tmp/vendor/lightbox/lightbox.js
@@ -27,7 +27,7 @@ var lightBox = function () {
 	this.buttonRight;
 	this.buttonLeft;
 	this.loadingAnimation;
-	this.html = '<div class="lightbox-background"><div class="lightbox-loader" title="2"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"><path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"><animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/></path></svg></div><div class="lightbox-slides"></div><div class="lightbox-exit"></div><div class="lightbox-buttons"><div class="lightbox-button-left lightbox-inactive"></div><div class="lightbox-button-right"></div></div></div>';
+	this.html = '<div id="lightbox-background"><div class="lightbox-loader" title="2"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"><path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"><animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/></path></svg></div><div class="lightbox-slides"></div><div class="lightbox-exit"></div><div class="lightbox-buttons"><div class="lightbox-button-left lightbox-inactive"></div><div class="lightbox-button-right"></div></div></div>';
 
 	this.initLightBox();
 };
@@ -164,7 +164,7 @@ lightBox.prototype.close = function() {
 	this.body.style.overflow = "auto";
 
 	//remove all lightbox
-	document.querySelector(".lightbox-background").remove();
+	document.getElementById("lightbox-background").remove();
 
 	//CLEAN
 	this.clean();
